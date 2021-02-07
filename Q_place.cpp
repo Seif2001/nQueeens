314a45e2ee0f604ebc58b21799d3a8a452e0df61
janashalabyy <janashalabyy@aucegypt.edugit clone https://github.com/Tanbouly5/Knapsack1.gitgit config --global user.email janashalabyy@aucegypt.edu>
2021-02-07 17:54:41 +0200

    message

---
+++ Q_place.cpp
@@ -0,0 +1,47 @@
+#include  "Q_place.h"
+int arr[30],counter=0;
+
+int Q_place(int pos) {
+
+int j;
+
+for (j=1;j<pos;j++) {
+
+if((arr[j]==arr[pos])||((abs(arr[j]-arr[pos])==abs(j-pos))))
+
+return 0;
+
+}
+
+return 1;
+
+}
+
+void print(int n) {
+
+int p,j;
+
+counter++;
+
+cout<<"\nSolution #"<<counter<<":\n";
+
+for (p=1;p<=n;p++) {
+
+for (j=1;j<=n;j++) {
+
+if(arr[p]==j)
+
+cout<<"Q\t";
+
+else
+
+cout<<"*\t";
+
+}
+
+cout<<"\n";
+
+}
+
+}
+
